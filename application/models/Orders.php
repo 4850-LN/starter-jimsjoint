57c4d6ad821d078ca16ad31e1d97b5e2c6e592dd
Nancy Ra <nancy.ra@live.ca>
2016-02-11 16:22:53 -0800

    fix 6: -validate() -checkout() Signed-off-by:Nancy Ra <nancy.ra@live.ca>

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -58,7 +58,17 @@ class Orders extends MY_Model {
     // validate an order
     // it must have at least one item from each category
     function validate($num) {
-        return false;
+        $CI = & get_instance();
+        $items = $CI->orderitems->group($num);
+        $gotten = array();
+        if(count($items)> 0)
+            foreach ($items as $item)
+            {
+                $menu = $CI->menu->get($item->item);
+                $gotten[$menu->category] = 1;
+            }
+            
+        return isset($gotten['m']) && isset($gotten['d']) && isset($gotten['s']);
     }
 
 }
